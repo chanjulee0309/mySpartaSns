674c4ba71ec7cf6a817b342feb8eab391715f251
chanjulee <crdlckswn@naver.com>
2023-04-15 22:15:16 +0900

    데이터베이스에 비밀번호데이터 암호화되어 저장 기능 추가

--- user/views.py
+++ user/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import UserModel
 from django.http import HttpResponse
+from django.contrib.auth import get_user_model
 
 
 # Create your views here.
@@ -16,16 +17,12 @@ def sign_up_view(request):
         if password != password2:
             return render(request, 'user/signup.html')
         else:
-            exist_user = UserModel.objects.filter(username=username)
-
+            exist_user = get_user_model().objects.filter(username=username)
             if exist_user:
                 return render(request, 'user/signup.html')
             else:
-                new_user = UserModel()
-                new_user.username = username
-                new_user.password = password
-                new_user.bio = bio
-                new_user.save()
+                UserModel.objects.create_user(
+                    username=username, password=password, bio=bio)
                 return redirect('/sign-in')
 
 
