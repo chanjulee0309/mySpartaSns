8b4a2296b14a2f391634b072e57eeedb1f3b748f
chanjulee <crdlckswn@naver.com>
2023-04-16 23:26:24 +0900

    게시글 삭제 기능 추가

--- tweet/urls.py
+++ tweet/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     # 127.0.0.1:8000 과 views.py 폴더의 home 함수 연결
     path('', views.home, name='home'),
     # 127.0.0.1:8000/tweet 과 views.py 폴더의 tweet 함수 연결
-    path('tweet/', views.tweet, name='tweet')
+    path('tweet/', views.tweet, name='tweet'),
+    path('tweet/delete/<int:id>', views.delete_tweet, name='delete-tweet'),
 ]
