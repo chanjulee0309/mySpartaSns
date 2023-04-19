3bb0a3cd02a8367fce3046d68b91a98866bae2a1
chanjulee <crdlckswn@naver.com>
2023-04-19 15:13:35 +0900

    settings.py에서 DEBUG, ALLOWED_HOSTS 수정

--- mySpartaSns/settings.py
+++ mySpartaSns/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&1yysb3fg575*=_n(cc42a5r0gj#8k8-$h(eiba7osfmzv9p36'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
